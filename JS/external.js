a1efb083460454c7a87abd39b994848adac85955
Horace Smith <horacesmith26@gmail.com>
2022-11-21 12:17:35 -0600

    prompt, response with confirm

---
+++ JS/external.js
@@ -0,0 +1,5 @@
+console.log("Hello from external link");
+alert("welcome to my website!")
+var userInput = prompt('What is your favorite color:');
+var confirmed = confirm('Are you sure you thats your favorite?');
+alert('Nice ' + userInput + ' is my favorite color too')
